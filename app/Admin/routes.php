3246eb751d6eb0392e14fb1f2326ca1a9f68cc38
largezhou <largezhou@hotmail.com>
2021-03-14 16:37:16 +0800

    接入 laravel-admin

---
+++ app/Admin/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
+    'as' => config('admin.route.prefix').'.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->get('/wechat-menu', 'HomeController@wechatMenu')->name('wechat-menu');
+
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>测试微信公众号菜单</title>
 </head>
-<body style="display: flex;">
-<div style="display: inline-flex;">{!! $menuHtml !!}</div>
-<div style="display: inline-flex;">
-    <img width="200" height="200" src="{{ asset('0.jpg') }}"/>
-    <h1>测试微信号</h1>
+<body>
+<div style="display: flex;">
+    <div>{!! $menuHtml !!}</div>
+    <div>
+        <h1>测试微信号</h1>
+        <img width="200" height="200" src="{{ asset('0.jpg') }}"/>
+    </div>
 </div>
+<h1><a href="/admin/wechat-menu">接入 laravel-admin 后台</a></h1>
+<p>帐号密码：admin</p>
 </body>
 </html>
